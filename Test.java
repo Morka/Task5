0dc80d39ff92af320f75f36e17144e2b1723f614
kanakodavis <matthiasgusenbauer@gmx.net>
2012-11-19 23:36:21 +0100

    Check out my problem please. :(

--- Test.java
+++ Test.java
@@ -22,6 +22,16 @@ public class Test {
 		//	System.out.println(ctime.shorter(etime));
 		//}
 		
+		//OrderedSet + Description Test
+		Description desc1 = new Description("I? I am a monument to all your sins.");
+		Description desc2 = new Description("This one is machine and nerve,\nand has its mind concluded.\nThis one is but flesh and faith,\nand is the more deluded.");
+		Description desc3 = new Description("There is much talk, and I have listened,\nthrough rock and metal and time.\nNow I shall talk, and you shall listen.");
+		Description desc4 = new Description("Silence fills the empty grave,\nnow that I have gone.\nBut my mind is not at rest,\nfor questions linger on.\nI will ask, and you will answer.");
+		Description desc5 = new Description("We exist together now,\ntwo corpses in one grave...");
+		OrderedSet<Description> ordSet = new OrderedSet<Description>();//WIE BITTE WAS? Das problem is glaub ich weil Description ja Shorter<String> implementiert aber in OrderedSet Shortert<T> 
+		//die schranke ist. Kann des wer loesen? I hab da grad an knopf im denken...I vermut es geht irgendwie mit wildcards aber leider wenn i a Shorter<?> mach in OrderedSet dann bugged da rest
+		//System.out.println(string4);
+		
 		OrderedMap<String, Double> test = new OrderedMap<String, Double>();
 		test.insert("hallo");
 	}
